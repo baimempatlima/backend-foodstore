1ae31bfd7d6fa5293b78277054c2866847ad5777
Baimempatlima <97828149+baimempatlima@users.noreply.github.com>
2022-04-12 20:32:23 +0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const { dbHost, dbPass, dbName, dbPort, dbUser } = require("../app/config");
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+mongoose.connect( `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?retryWrites=true&w=majority`);
 const db = mongoose.connection;
 
 module.exports = db;
